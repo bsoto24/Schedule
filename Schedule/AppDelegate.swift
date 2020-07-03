5c6b9fb7fcf12feb6dd60fe4e14c830f8a6b4933
Bryam Soto <bryam.soto@avantica.net>
2020-07-02 19:09:47 -0500

    Initial Commit

---
+++ Schedule/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Schedule
+//
+//  Created by Bryam Soto Ñahui on 7/2/20.
+//  Copyright © 2020 OpenLab. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Schedule/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Schedule
+//
+//  Created by Bryam Soto Ñahui on 7/2/20.
+//  Copyright © 2020 OpenLab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
